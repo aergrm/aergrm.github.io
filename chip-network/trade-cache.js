2c10708a4d6b5d105f5f250a394127ca23a2f9a4
aergrm <116939278+aergrm@users.noreply.github.com>
2026-07-31 00:12:53 -0500

    Add local trade cache

---
+++ chip-network/trade-cache.js
@@ -0,0 +1,7 @@
+// This file is refreshed by .github/workflows/update-chip-trade.yml.
+const TRADE_CACHE = {
+  "updated": null,
+  "source": "UN Comtrade public API",
+  "commodity": "HS 8542 integrated circuits",
+  "data": {}
+};
